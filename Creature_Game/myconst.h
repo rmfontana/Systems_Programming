855e11cdf374f7f151f7ebb0d200ca0f8a462461
Rose M Fontana <rfontana@oswego.edu>
2020-06-24 05:03:55 -0400

    Create myconst.h

---
+++ Creature_Game/myconst.h
@@ -0,0 +1,28 @@
+const int roomNorth = 0;
+const int roomSouth = 1;
+const int roomEast = 2;
+const int roomWest = 3;
+
+const char* room0 = "north";
+const char* room1 = "south";
+const char* room2 = "east";
+const char* room3 = "west";
+
+const char* state0 = "clean";
+const char* state1 = "half-dirty";
+const char* state2 = "dirty";
+
+const int player = 0;
+const int animal = 1;
+const int human = 2;
+
+const char* type0 = "player";
+const char* type1 = "animal";
+const char* type2 = "human";
+
+const char* humanMad = "grumbled";
+const char* humanHappy = "smiled";
+const char* animalMad = "growled";
+const char* animalHappy = "licked your face";
+
+const char* colon = ":";
